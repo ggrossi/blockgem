00c564d358aeed45e0f926e23b9beb56ca65207b
Gustavo Grossi <grossigus@gmail.com>
2023-07-06 13:46:03 -0300

    Initial Commit

---
+++ src/util/prices.js
@@ -0,0 +1,19 @@
+// Map our custom plan IDs ("basic", "premium", etc) to Stripe price IDs
+const stripePriceIds = {
+  monthly: process.env.NEXT_PUBLIC_STRIPE_PRICE_MONTHLY,
+  yearly: process.env.NEXT_PUBLIC_STRIPE_PRICE_YEARLY,
+  lifetime: process.env.NEXT_PUBLIC_STRIPE_PRICE_LIFETIME,
+};
+
+// Get Stripe priceId
+export function getStripePriceId(planId) {
+  return stripePriceIds[planId];
+}
+
+// Get friendly plan ID ("basic", "premium", etc) by Stripe plan ID
+// Used in auth.js to include planId in the user object
+export function getFriendlyPlanId(stripePriceId) {
+  return Object.keys(stripePriceIds).find(
+    (key) => stripePriceIds[key] === stripePriceId
+  );
+}
